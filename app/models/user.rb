2cda27567c271452c2b2f3c029ab6f4910d1994a
Deniz-Iren <deniz5734@hotmail.com>
2021-10-29 23:24:47 +0300

    Fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+  
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy, inverse_of: :follower
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy, inverse_of: :followed
 
